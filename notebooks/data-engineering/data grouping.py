24794fd0d87575f35ff84dee225b773c54d2d268
Daria-Sonne <dguganova@gmail.com>
2025-12-09 19:58:37 +0300

    Changed grouping by industries and sectors

--- notebooks/data-engineering/data grouping.py
+++ notebooks/data-engineering/data grouping.py
@@ -3,17 +3,12 @@ import pandas as pd
 # 1. Загружаем отфильтрованный файл с секторами и индустриями
 df_filtered = pd.read_csv("../../data/preprocessed/sectors_industry_filtered.csv")
 
-# Проверим, как называются колонки
-print("Колонки в файле:", df_filtered.columns.tolist())
-
 # 2. Группировка по Sector
-tickers_by_sector = df_filtered.groupby('Sector')['Ticker'].apply(lambda x: ','.join(x)).reset_index()
-tickers_by_sector_file = "../../data/preprocessed/tickers_by_sector.csv"
-tickers_by_sector.to_csv(tickers_by_sector_file, index=False)
-print(f"Сохранен файл с тикерами по секторам: {tickers_by_sector_file}")
+tickers_by_sector = df_filtered.groupby('sector')['ticker'].apply(lambda x: ','.join(x)).reset_index()
+tickers_by_sector.to_csv("../../data/preprocessed/tickers_by_sector.csv", index=False)
+print(f"Сохранен файл с тикерами по секторам")
 
 # 3. Группировка по Industry
-tickers_by_industry = df_filtered.groupby('Industry')['Ticker'].apply(lambda x: ','.join(x)).reset_index()
-tickers_by_industry_file = "../../data/preprocessed/tickers_by_industry.csv"
-tickers_by_industry.to_csv(tickers_by_industry_file, index=False)
-print(f"Сохранен файл с тикерами по индустриям: {tickers_by_industry_file}")
+tickers_by_industry = df_filtered.groupby('industry')['ticker'].apply(lambda x: ','.join(x)).reset_index()
+tickers_by_industry.to_csv("../../data/preprocessed/tickers_by_industry.csv", index=False)
+print(f"Сохранен файл с тикерами по индустриям")
